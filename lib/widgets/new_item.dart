c4131b164942484e642e8b942e0407d065fc6e09
adala-wanyande <benardwanyande@gmail.com>
2024-02-12 07:35:46 +0300

    feat: fetch and transform data from the backend

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -42,9 +42,6 @@ class _NewItemState extends State<NewItem> {
         ),
       );
 
-      print(response.body);
-      print(response.statusCode);
-
       if (!context.mounted) {
         return;
       }
